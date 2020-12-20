8208099463205a9cf4f258c51c52782d484108ad
mitkan <76113842+mitkan@users.noreply.github.com>
2020-12-20 21:28:01 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function clickbutton(val)
+{
+  document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+function clearDisplay(){
+    document.getElementById("screen").value="";
+}
+function equalClick()
+{
+    var text=document.getElementById("screen").value;
+    var result=eval(text);
+    document.getElementById("screen").value=result;// here 'eval' is a function which performs evaluation function of an expression in javascript
+}
